9aef5202450bd41811f5cf3d877166f92acd7fb1
VvSs79 <vitaliysokolov1@gmail.com>
2023-03-27 18:10:06 +0300

    global refactoring

---
+++ src/main/resources/postgresql/dml/add.sql
@@ -0,0 +1,6 @@
+INSERT INTO fitness.role(role) VALUES ('ADMIN');
+INSERT INTO fitness.role(role) VALUES ('USER');
+
+INSERT INTO fitness.status(status) VALUES ('ACTIVATED');
+INSERT INTO fitness.status(status) VALUES ('DEACTIVATED');
+INSERT INTO fitness.status(status) VALUES ('WAITING_ACTIVATION');
